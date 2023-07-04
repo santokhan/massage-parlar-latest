5c8f94b1aa8adf8423fbccdf4e0d25054c761185
Santo <santokhan1999@gmail.com>
2023-07-04 06:41:22 -0700

    Initial commit

--- src/components/products/layout/PLayout.js
+++
@@ -1,7 +0,0 @@
-export default function PLayout({ children }) {
-  return (
-    <div className="py-8 lg:pt-16 lg:pb-8">
-      <div className="max-w-7xl mx-auto">{children}</div>
-    </div>
-  );
-}
--- src/components/products/layout/PMain.js
+++
@@ -1,5 +0,0 @@
-export default function PMain({ children }) {
-  return (
-    <div className="flex flex-wrap justify-center gap-12 py-10">{children}</div>
-  );
-}
