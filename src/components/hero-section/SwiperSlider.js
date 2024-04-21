cd1ee9ea0bef15690e9ae4ea8a49eefa0c29bf15
Santo Khan <santokhan1999@gmail.com>
2024-04-21 16:52:58 +0600

    update slider

--- src/components/hero-section/SwiperSlider.js
+++ src/components/hero-section/SwiperSlider.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 
 import "swiper/css";
@@ -10,41 +10,36 @@ import { Pagination, Autoplay, Navigation } from "swiper";
 import HeroSection from "./HeroSection";
 import HeroSection1 from "./HeroSection1";
 import HeroSection2 from "./HeroSection2";
-import headerImg from '../../assets/images/ahmassage-header.jpg'
+import SliderProvider, { SliderContext } from '../../context/sliderContext.js'
 
-function SwiperSlider({ startScroll }) {
+function SwiperSliderStatic() {
   return (
-    <>
-      <Swiper
-        spaceBetween={0}
-        centeredSlides={true}
-        autoplay={{
-          delay: 6000,
-          disableOnInteraction: false,
-        }}
-        pagination={{
-          clickable: true,
-        }}
-        loop={true}
-        navigation={true}
-        modules={[Pagination]}
-        className="mySwiper"
-      >
-        <SwiperSlide>
-          <HeroSection startScroll={startScroll}/>
-        </SwiperSlide>
-        <SwiperSlide>
-          <HeroSection1 startScroll={startScroll}/>
-        </SwiperSlide>
-      </Swiper>
-    </>
-  );
+    <Swiper
+      spaceBetween={0}
+      centeredSlides={true}
+      autoplay={{
+        delay: 6000,
+        disableOnInteraction: false,
+      }}
+      pagination={{
+        clickable: true,
+      }}
+      loop={true}
+      navigation={true}
+      modules={[Pagination]}
+      className="mySwiper"
+    >
+      <SwiperSlide>
+        <HeroSection />
+      </SwiperSlide>
+      <SwiperSlide>
+        <HeroSection1 />
+      </SwiperSlide>
+      <SwiperSlide>
+        <HeroSection2 />
+      </SwiperSlide>
+    </Swiper>
+  )
 }
 
-const HeaderImage = () => {
-  return <>
-    <img src={headerImg} alt="header" className="w-full min-h-svh object-cover" />
-  </>;
-};
-
-export default SwiperSlider;
+export default SwiperSliderStatic;
